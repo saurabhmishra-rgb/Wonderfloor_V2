921ee478dc30b8558ef2530e7bc6ca73c05657f3
saurabhmishra-rgb <114287098+saurabhmishra-rgb@users.noreply.github.com>
2026-05-21 15:42:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
+// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import MobileUpload from "./components/MobileUpload.jsx"; // <-- Import the new mobile page
+import MobileUpload from "./components/MobileUpload.jsx"
 import './index.css'
-
-// Check the URL in the browser
-const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* If the URL is exactly '/mobile-upload', show the mobile page. Otherwise, show the main laptop App. */}
-    {path === '/mobile-upload' ? <MobileUpload /> : <App />}
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/mobile-upload" element={<MobileUpload />} />
+        
+        <Route path="/app" element={<App />} />
+        
+        {/* ADD THE DYNAMIC PATH VARIABLES HERE */}
+        <Route path="/visualizer/:productId/:roomId" element={<App />} /> 
+        
+        {/* Optional: Keep this one too just in case someone types only /visualizer */}
+        <Route path="/visualizer" element={<App />} /> 
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
