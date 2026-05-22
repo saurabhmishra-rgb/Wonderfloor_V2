375ab1281e4d7c24260ed0f5ab6ee53cf2d107f6
saurabhmishra-rgb <114287098+saurabhmishra-rgb@users.noreply.github.com>
2026-05-22 10:55:45 +0530

    Update ARVisualizer.jsx

--- src/components/ARVisualizer.jsx
+++ src/components/ARVisualizer.jsx
@@ -1528,7 +1528,7 @@ if (sortOrder === 'Prod-A-Z') {
                 <div className="border-t border-gray-200 flex flex-col">
                   {[
                     { label: 'SKU', value: detailsProduct.sku || detailsProduct.name },
-                    { label: 'Collection/ Style', value: detailsProduct.collection },
+                    { label: 'Style', value: detailsProduct.collection },
                     { label: 'Flooring Product', value: detailsProduct.category },
                     { label: 'Colour', value: detailsProduct.colour },
                     { label: 'Shade', value: detailsProduct.shade },
