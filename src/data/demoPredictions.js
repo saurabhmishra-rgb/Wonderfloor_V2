abbd63216ca5a6938efcced2fddd6192025f03a8
saurabhmishra-rgb <114287098+saurabhmishra-rgb@users.noreply.github.com>
2026-07-24 10:20:58 +0530

    Create demoPredictions.js

---
+++ src/data/demoPredictions.js
@@ -0,0 +1,250 @@
+export const demoPredictions = {
+  // Industrial Flooring - Office
+  "6a4606084ca404204c794cae": {
+    output: {
+      prediction: {
+        camera: {
+          cx: 800,
+          cy: 599.9999389648438,
+          fx: 783.1760864257812,
+          fy: 783.1761474609375,
+          height_meters: 1.5129698645074483,
+          pitch_degrees: -0.9376220089980224,
+          roll_degrees: 0.1845383444800954
+        },
+        floor: {
+          plane: {
+            distance: 1.5129698645074483,
+            normal: [0.004065696236456458, -0.9988972012104483, -0.046774475176596615]
+          }
+        },
+        input: {
+          height: 1200,
+          width: 1600
+        }
+      }
+    }
+  },
+
+  // Industrial Flooring - 2
+  "6a4603c2b7a9facb9cc3fee1": {
+    output: {
+      prediction: {
+        camera: {
+          cx: 800,
+          cy: 599.9999389648438,
+          fx: 783.1760864257812,
+          fy: 783.1761474609375,
+          height_meters: 1.5129698645074483,
+          pitch_degrees: -0.9376220089980224,
+          roll_degrees: 0.1845383444800954
+        },
+        floor: {
+          plane: {
+            distance: 1.5129698645074483,
+            normal: [0.004065696236456458, -0.9988972012104483, -0.046774475176596615]
+          }
+        },
+        input: {
+          height: 1200,
+          width: 1600
+        }
+      }
+    }
+  },
+
+  // School
+ // 1. School Room
+  "6a46243bac0c56bac243c30f": {
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 3364.81103515625,
+      fy: 3364.810791015625,
+      height_meters: 1.9345139799407232,
+      pitch_degrees: -9.123054646082059,
+      roll_degrees: 2.1500398172219946
+    },
+    floor: {
+      plane: {
+        distance: 1.9345139799407232,
+        normal: [-0.015559538536334691, -0.9850955249388406, -0.17130296992757144]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  },
+
+  // 2. Sport Room (Space Removed & Cleaned)
+  "6a462dcf12f464296f8a98e3": {
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 1997.2353515625,
+      fy: 1997.2353515625,
+      height_meters: 0.5246691433913914,
+      pitch_degrees: 11.074311625907765,
+      roll_degrees: -15.118285633459308
+    },
+    floor: {
+      plane: {
+        distance: 0.5246691433913914,
+        normal: [
+          0.047827274787396644,
+          -0.8613987291088183,
+          -0.505672602852996
+        ]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  },
+  //Supermarket
+  "6a462bd212f464296f8a98e1": {
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 1143.2479248046875,
+      fy: 1143.248046875,
+      height_meters: 1.4654849378099148,
+      pitch_degrees: -19.269657535520135,
+      roll_degrees: 1.877650805904828
+    },
+    floor: {
+      plane: {
+        distance: 1.4654849378099148,
+        normal: [
+          -0.028700481684608817,
+          -0.9450310611045352,
+          -0.32571855320001053
+        ]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  },
+  // Supermarket
+  "6a462d6b12f464296f8a98e2":{
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 4127.2392578125,
+      fy: 4127.2392578125,
+      height_meters: 1.2146296932666043,
+      pitch_degrees: -9.028500922306987,
+      roll_degrees: 0.5490831228847034
+    },
+    floor: {
+      plane: {
+        distance: 1.2146296932666043,
+        normal: [
+          0.019859311390768526,
+          -0.989394940145663,
+          -0.14388627511074306
+        ]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  },
+  //Hospital
+  "6a4602dcb7a9facb9cc3fee0": {
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 1563.1451416015625,
+      fy: 1563.1453857421875,
+      height_meters: 1.547279667290866,
+      pitch_degrees: -9.982066193653406,
+      roll_degrees: 2.1812437498886093
+    },
+    floor: {
+      plane: {
+        distance: 1.547279667290866,
+        normal: [
+          -0.03658492185766121,
+          -0.97872795663078,
+          -0.2018740458850684
+        ]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  },
+  //Hotel and Hospitality
+  "6a3bc82089199742512b7e05": {
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 5622.7958984375,
+      fy: 5622.7958984375,
+      height_meters: 1.405296044133009,
+      pitch_degrees: -4.163045976012846,
+      roll_degrees: 0.5149687825797227
+    },
+    floor: {
+      plane: {
+        distance: 1.405296044133009,
+        normal: [
+          -0.009050158933070398,
+          -0.9976892753004148,
+          -0.06733650253628609
+        ]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  },
+  //Prefab
+  "6a461b34d0ce9cfc46e4093a": {
+    camera: {
+      cx: 800,
+      cy: 599.9999389648438,
+      fx: 782.8927612304688,
+      fy: 782.8927001953125,
+      height_meters: 1.5130481542925371,
+      pitch_degrees: -0.934124418534447,
+      roll_degrees: 0.17171737237668067
+    },
+    floor: {
+      plane: {
+        distance: 1.5130481542925371,
+        normal: [
+          0.004043106510082151,
+          -0.9988950670652053,
+          -0.04682198503424773
+        ]
+      }
+    },
+    images: {
+      input: {
+        height: 1200,
+        width: 1600
+      }
+    }
+  }
+};
